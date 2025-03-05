d3e6dbf69fbf612956775259831d1e97c2d744dd
Franco <freconur@gmail.com>
2025-03-04 20:31:32 -0500

    cambio logo de sidebar

--- components/sidebar/SidebarRegional.tsx
+++ components/sidebar/SidebarRegional.tsx
@@ -2,19 +2,21 @@ import { useGlobalContext } from '@/features/context/GlolbalContext'
 import { regionTexto } from '@/fuctions/regiones'
 import Image from 'next/image'
 import React from 'react'
+import logo from '../../assets/formativa-logo.png'
 
 const SidebarRegional = () => {
   const { currentUserData } = useGlobalContext()
   return (
-    <div className='border-b-[1px] border-slate-200 pb-5 mb-5 '>
+    <div className='border-b-[1px] border-slate-200 pb-2 '>
       <h1 className='uppercase text-lg text-center font-dmMono text-slate-600'>dirección regional de educación puno</h1>
       <div className='flex justify-center items-center'>
         <Image
           alt="foto de perfil"
-          src={`https://firebasestorage.googleapis.com/v0/b/attendance-system-d1f40.appspot.com/o/ugel-huancane.png?alt=media&token=21404b68-c541-4d27-b8d5-173729324f9a`}
+          src={logo}
+          // src={`https://firebasestorage.googleapis.com/v0/b/attendance-system-d1f40.appspot.com/o/ugel-huancane.png?alt=media&token=21404b68-c541-4d27-b8d5-173729324f9a`}
           // fill
-          width={50}
-          height={50}
+          width={70}
+          height={70}
         />
       </div>
       <p className='capitalize text-sm text-center p-2 text-slate-500 text-md '>
