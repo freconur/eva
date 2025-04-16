66c45141ca4f150360d1095ed61a780d3c046b98
franco <freconur@gmail.com>
2025-04-15 19:14:21 -0500

    agregando el sidebar para directores y administradores

--- components/sidebar/SidebarAdmin.tsx
+++ components/sidebar/SidebarAdmin.tsx
@@ -6,6 +6,10 @@ import SidebarRegional from './SidebarRegional';
 import { useRolUsers } from '@/features/hooks/useRolUsers';
 import SidebarRegion from './SidebarRegion';
 import BackgroundSidebar from './background-sidebar';
+import styles from './sideBarList.module.css'
+import { MdAccountBalance, MdAccountCircle } from 'react-icons/md';
+import { FaUserGraduate, FaUserNinja, FaUserTie } from 'react-icons/fa';
+import { LuListTodo } from "react-icons/lu";
 interface Props {
   showSidebar: boolean
 }
@@ -21,7 +25,92 @@ const SidebarAdmin = ({ showSidebar }: Props) => {
       <div onClick={() => showSidebarValue(showSidebar)} className='absolute capitalize p-2 w-[30px] h-[30px] bg-colorBrand1 justify-center items-center flex rounded-full text-white l-10 cursor-pointer left-[235px] top-[30px] shadow-md'>x</div>
       <div>
         <SidebarRegional />
-        <ul className='capitalize font-montserrat border-b-[1px] border-slate-200 pb-5 mb-5'>
+        <div className="relative z-[100] flex  items-center ml-[17px] my-[10px]">
+
+          <MdAccountCircle className={styles.icons} />
+          <Link className={styles.ancla} href="/mi-cuenta" aria-haspopup="true">
+            Mi cuenta
+            {/* <label>Estudiantes</label> */}
+          </Link>
+        </div>
+        <div className={styles.containerLista}></div>
+        <div className={styles.test} role="navigation">
+          <ul className={styles.containerMenu} >
+            <li className={styles.dropdown}>
+              <div className={styles.containerIcon}>
+
+                <LuListTodo className={styles.icons} />
+                <Link className={styles.ancla} href="#" aria-haspopup="true">
+                  Especialistas
+                  {/* <label>Estudiantes</label> */}
+                </Link>
+              </div>
+              <ul className={styles.dropdownContent} aria-label="submenu">
+                <li className={styles.containerAncla}><Link
+                  href="/admin/agregar-especialista"
+                  className={styles.anclaje}
+                  id="ancla">crear usuario</Link></li>
+              </ul>
+            </li>
+            <li className={styles.dropdown}>
+              <div className={styles.containerIcon}>
+
+                <MdAccountBalance className={styles.icons} />
+                <Link className={styles.ancla} href="#" aria-haspopup="true">
+                  Directores
+                  {/* <label>Estudiantes</label> */}
+                </Link>
+              </div>
+              <ul className={styles.dropdownContent} aria-label="submenu">
+                <li className={styles.containerAncla}><Link
+                  href="/admin/agregar-directores"
+                  className={styles.anclaje}
+                  id="ancla">crear usuario</Link></li>
+              </ul>
+            </li>
+            <li className={styles.dropdown}>
+              <div className={styles.containerIcon}>
+
+                <FaUserTie className={styles.icons} />
+                <Link className={styles.ancla} href="#" aria-haspopup="true">
+                  Docentes
+                  {/* <label>Estudiantes</label> */}
+                </Link>
+              </div>
+              <ul className={styles.dropdownContent} aria-label="submenu">
+                <li className={styles.containerAncla}><Link
+                  href="/directores/evaluaciones-docentes"
+                  className={styles.anclaje}
+                  id="ancla">Evaluaciones</Link></li>
+                <li className={styles.containerAncla}><Link
+                  href="/directores/agregar-profesores"
+                  id="ancla"
+                  className={styles.anclaje} >Crear usuario</Link></li>
+              </ul>
+            </li>
+            <li className={styles.dropdown}>
+              <div className={styles.containerIcon}>
+
+                <FaUserGraduate className={styles.icons} />
+                <Link className={styles.ancla} href="#" aria-haspopup="true">
+                  Estudiantes
+                  {/* <label>Estudiantes</label> */}
+                </Link>
+              </div>
+              <ul className={styles.dropdownContent} aria-label="submenu">
+                <li className={styles.containerAncla}><Link
+                  href="/admin/evaluaciones"
+                  className={styles.anclaje}
+                  id="ancla">Evaluaciones</Link></li>
+                <li className={styles.containerAncla}><Link
+                  href="/admin/agregar-evaluaciones"
+                  id="ancla"
+                  className={styles.anclaje} >Agregar evaluacion</Link></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+        {/* <ul className='capitalize font-montserrat border-b-[1px] border-slate-200 pb-5 mb-5'>
           <li onClick={() => showSidebarValue(showSidebar)} className='rounded-md text-white pl-2 text-sm flex items-center gap-x-4 cursor-pointer   mt-2 capitalize   hover:border-[1px] hover:border-violet-300 hover:text-violet-300 duration-300 mx-2 whitespace-nowrap drop-shadow-lg'>
             <Link href="/mi-cuenta" className='p-3'>
               Mi cuenta
@@ -47,7 +136,7 @@ const SidebarAdmin = ({ showSidebar }: Props) => {
               Agregar evaluación
             </Link>
           </li>
-        </ul>
+        </ul> */}
         <SidebarInfoUser showSidebar={showSidebar} />
       </div>
       <div onClick={logout} className="relative z-[10] ml-2 border rounded-sm border-white  duration-300 hover:border-red-300 p-3 h-[50px] font-montserrat hover:text-red-300 text-white w-[200px] cursor-pointer ">
